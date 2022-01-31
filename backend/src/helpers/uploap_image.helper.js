5bf889d90c89d5acdf2954750e2b72a4b312caa1
camora1990 <moorales.camilo@gmail.com>
2022-01-31 11:27:06 -0500

    helper upload product images

--- backend/src/helpers/uploap_image.helper.js
+++ backend/src/helpers/uploap_image.helper.js
@@ -36,6 +36,59 @@ const uploadImage = (files, imgIs = "employee") => {
   });
 };
 
+/**
+ * @description this function upload images in local folder products
+ * @param {*} files
+ * @returns
+ * @author Camilo Morales Sanchez
+ */
+const uploadProductImages = (files) => {
+  return new Promise(async (resolve, reject) => {
+    let nameImages = [];
+    const { image } = files;
+    if (!Array.isArray(image)) {
+      try {
+        const imageName = await uploadImage(files, "products");
+        nameImages.push(imageName);
+        resolve(nameImages);
+        return;
+      } catch (error) {
+        reject(error);
+        return;
+      }
+    }
+    try {
+      image.forEach((ele) => {
+        const splitImageName = ele.name.split(".");
+        const imageExtension = splitImageName[splitImageName.length - 1];
+        const temImg = `${uuidv4()}.${imageExtension}`;
+        if (!validExtensions.includes(imageExtension)) {
+          throw new Error(
+            `Invalid extension, allowed extension are: [${validExtensions}]`
+          );
+        }
+        const senToPah = path.join(__dirname, "../storage/products/", temImg);
+        nameImages.push(temImg);
+        ele.mv(senToPah, (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+        });
+      });
+      resolve(nameImages);
+    } catch (error) {
+      if (!nameImages.isEmpty()) {
+        nameImages.forEach(async (element) => {
+          await deleteImgLocal(element, "../storage/products/");
+        });
+      }
+      reject(error);
+    }
+  });
+};
+
 module.exports = {
   uploadImage,
+  uploadProductImages,
 };
