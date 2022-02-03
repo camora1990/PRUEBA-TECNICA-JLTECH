09faf0e6841bc73e09fb9f1bb7b082703c9809a2
camora1990 <moorales.camilo@gmail.com>
2022-02-02 19:16:25 -0500

    using EmployeeProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import axios from "axios";
+import { EmployeeProvider } from "./context/EmployeeContext";
+axios.defaults.baseURL = "http://localhost:8081/api/v1";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <EmployeeProvider>
+    <div className="font-monospace">
+      <App />
+    </div>
+  </EmployeeProvider>,
+
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
