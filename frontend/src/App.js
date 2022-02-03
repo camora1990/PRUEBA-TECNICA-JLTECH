010f774725fc7a1fab0322e84ad4a8dbae25e820
camora1990 <moorales.camilo@gmail.com>
2022-02-02 19:42:51 -0500

    login public route is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-
+import { Login } from "./components/Login";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
+import { useEmployee } from "./context/EmployeeContext";
 function App() {
+  const {employee} = useEmployee()
+  const Public =(props)=>{
+    return employee.login? <Redirect to="/temporal" />: <Route {...props} />
+  }
+
   return (
-    <>
-    <h1>APP</h1>
-    </>
-  )
+    <Router>
+      <Public path="/" exact component={Login} />
+    </Router>
+  );
 }
 
 export default App;
